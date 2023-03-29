93dfe869a0d19ceeda4ee0ed00ad379a57fc2161
OBrien-T <obrienadedapo15@gmail.com>
2023-03-29 03:28:54 +0100

    Added Interactiveness

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+let navlink = document.querySelector('.navlinks');
+
+let body = document.querySelector("body");
+
+let sectionTwoCardContainer = document.querySelector('.section2ImageBackgroundContainer')
+
 const handleShowSidebar = () => {
-  console.log("aptlearn");
+  navlink.classList.add('navlinksShown');
+
+  body.classList.add('bodyUnscroll')
 };
+
+
+const handleRemoveSideBar = () => {
+  navlink.classList.remove('navlinksShown');
+
+  body.classList.remove('bodyUnscroll')
+}
+
+const handleScrollRight = () => {
+  sectionTwoCardContainer.scrollBy(500, 0);
+}
+const handleScrollLeft = () => {
+  sectionTwoCardContainer.scrollBy(-500, 0);
+}
